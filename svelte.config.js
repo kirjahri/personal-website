156a269d0909943cf895a945ab24ac8d8ccc1555
Kirjahri <150558369+kirjahri@users.noreply.github.com>
2025-04-05 22:06:52 -0400

    feat: add and use PostCSS

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,20 @@
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { sveltePreprocess } from 'svelte-preprocess';
 
 const config = {
-	preprocess: [vitePreprocess(), mdsvex()],
+	preprocess: [
+		vitePreprocess(),
+		mdsvex(),
+		sveltePreprocess({
+			postcss: {
+				plugins: {
+					autoprefixer: {}
+				}
+			}
+		})
+	],
 	kit: { adapter: adapter() },
 	extensions: ['.svelte', '.svx']
 };
