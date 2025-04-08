410d7a2855731014f0a172c92e7854022580e6b1
Kirjahri <150558369+kirjahri@users.noreply.github.com>
2025-04-08 00:17:20 -0400

    feat(/): add Neocities badge to header

--- src/lib/components/home/Header.svelte
+++ src/lib/components/home/Header.svelte
@@ -1,8 +1,15 @@
+<script lang="ts">
+	import neocitiesBadge from '$lib/assets/neocities-badge.svg';
+</script>
+
 <header
-	class="rounded-[5px] rounded-b-none border-b-2 border-lime-800 bg-lime-800 bg-gradient-to-b from-transparent to-white/20 p-2 inset-shadow-sm inset-shadow-white/20"
+	class="flex justify-between rounded-[5px] rounded-b-none border-b-2 border-lime-800 bg-lime-800 bg-gradient-to-b from-transparent to-white/20 p-2 inset-shadow-sm inset-shadow-white/20"
 >
-	<h1 class="text-3xl font-medium text-white uppercase">Kirjahri's Website</h1>
-	<h2 class="font-mono font-medium text-white/50">
-		<a href="/" class="hover:text-white hover:underline">kirja.me</a>
-	</h2>
+	<div>
+		<h1 class="text-3xl font-medium text-white uppercase">Kirjahri's Website</h1>
+		<h2 class="font-mono font-medium text-white/50">
+			<a href="/" class="hover:text-white hover:underline">kirja.me</a>
+		</h2>
+	</div>
+	<img src={neocitiesBadge} alt="Neocities badge" class="float-right" />
 </header>
