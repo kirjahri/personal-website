07acddac4ab9ee0a2d17645e54ecb298e1824124
Kirjahri <150558369+kirjahri@users.noreply.github.com>
2025-04-08 00:23:50 -0400

    fix(/): Neocities badge improperly aligned

--- src/lib/components/home/Header.svelte
+++ src/lib/components/home/Header.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <header
-	class="flex justify-between rounded-[5px] rounded-b-none border-b-2 border-lime-800 bg-lime-800 bg-gradient-to-b from-transparent to-white/20 p-2 inset-shadow-sm inset-shadow-white/20"
+	class="flex items-center justify-between rounded-[5px] rounded-b-none border-b-2 border-lime-800 bg-lime-800 bg-gradient-to-b from-transparent to-white/20 p-2 inset-shadow-sm inset-shadow-white/20"
 >
 	<div>
 		<h1 class="text-3xl font-medium text-white uppercase">Kirjahri's Website</h1>
