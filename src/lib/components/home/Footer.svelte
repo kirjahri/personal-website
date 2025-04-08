d7ff0461113d7384008d32021bcf30b76bc34562
Kirjahri <150558369+kirjahri@users.noreply.github.com>
2025-04-08 00:01:11 -0400

    feat(/): add github repository link to footer

--- src/lib/components/home/Footer.svelte
+++ src/lib/components/home/Footer.svelte
@@ -1,3 +1,6 @@
 <footer class="rounded-lg text-center text-white">
-	&copy; {new Date().getFullYear()} kirja.me
+	&copy; {new Date().getFullYear()} kirja.me &bull;
+	<a href="https://github.com/kirjahri/personal-website" target="_blank" class="hover:underline"
+		>Source code</a
+	>
 </footer>
