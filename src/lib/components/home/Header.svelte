7c948bbe216e1b31d6eb8e42b8133596c45dede2
Kirjahri <150558369+kirjahri@users.noreply.github.com>
2025-04-10 18:36:40 -0400

    feat(/): create component for Neocities badge

--- src/lib/components/home/Header.svelte
+++ src/lib/components/home/Header.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
-	import neocitiesBadge from '$lib/assets/neocities-badge.svg';
+	import NeocitiesBadge from './NeocitiesBadge.svelte';
 </script>
 
 <header
 	class="flex items-center justify-between rounded-[5px] rounded-b-none border-b-2 border-lime-800 bg-lime-800 bg-gradient-to-b from-transparent to-white/20 p-2 inset-shadow-sm inset-shadow-white/20"
 >
 	<h1 class="font-mono text-3xl font-medium text-white uppercase">Kirjahri's Website</h1>
-	<a href="https://neocities.org/site/kirjahri" target="_blank">
-		<img src={neocitiesBadge} alt="Neocities badge" class="float-right" />
-	</a>
+	<NeocitiesBadge />
 </header>
---
+++ src/lib/components/home/NeocitiesBadge.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import src from '$lib/assets/neocities-badge.svg';
+</script>
+
+<a href="https://neocities.org/site/kirjahri" target="_blank">
+	<img {src} alt="Neocities badge" />
+</a>
