087ccc56b0552435b7f3ffc203488bef153dbd73
Kirjahri <150558369+kirjahri@users.noreply.github.com>
2025-04-10 18:11:15 -0400

    feat(/): change header to use monospace font and delete subheader

--- src/lib/components/home/Header.svelte
+++ src/lib/components/home/Header.svelte
@@ -5,12 +5,7 @@
 <header
 	class="flex items-center justify-between rounded-[5px] rounded-b-none border-b-2 border-lime-800 bg-lime-800 bg-gradient-to-b from-transparent to-white/20 p-2 inset-shadow-sm inset-shadow-white/20"
 >
-	<div>
-		<h1 class="text-3xl font-medium text-white uppercase">Kirjahri's Website</h1>
-		<h2 class="font-mono font-medium text-white/50">
-			<a href="/" class="hover:text-white hover:underline">kirja.me</a>
-		</h2>
-	</div>
+	<h1 class="font-mono text-3xl font-medium text-white uppercase">Kirjahri's Website</h1>
 	<a href="https://neocities.org/site/kirjahri" target="_blank">
 		<img src={neocitiesBadge} alt="Neocities badge" class="float-right" />
 	</a>
