71706517fca55fcf758d198d377895f677ed6f20
Kirjahri <150558369+kirjahri@users.noreply.github.com>
2025-04-20 08:20:07 -0400

    feat(/): `font-mono font-medium` -> `font-semibold`

--- src/lib/components/home/Header.svelte
+++ src/lib/components/home/Header.svelte
@@ -5,6 +5,6 @@
 <header
 	class="flex items-center justify-between rounded-[5px] rounded-b-none border-b-2 border-lime-800 bg-lime-800 bg-gradient-to-b from-transparent to-white/20 p-2 inset-shadow-sm inset-shadow-white/20"
 >
-	<h1 class="font-mono text-3xl font-medium text-white uppercase">Kirjahri's Website</h1>
+	<h1 class="text-3xl font-semibold text-white uppercase">Kirjahri's Website</h1>
 	<NeocitiesBadge />
 </header>
